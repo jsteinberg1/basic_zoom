d49dfa00e85caa0689b9a7340c441bf411c27cfe
Justin Steinberg <jsteinberg@gmail.com>
2022-04-17 23:54:26 -0700

    bump version for rich support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="basic_zoom",
-    version="0.0.3",
+    version="0.0.4",
     author="Justin Steinberg",
     author_email="jsteinberg@gmail.com",
     install_requires=["requests", "PyJWT", "requests_oauthlib"],
