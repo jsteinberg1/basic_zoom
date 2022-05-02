6eca1433fad8f5a264ddcdedd8483e7d074342b1
Justin Steinberg <jsteinberg@gmail.com>
2022-05-01 20:57:18 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="basic_zoom",
-    version="0.0.4",
+    version="0.0.5",
     author="Justin Steinberg",
     author_email="jsteinberg@gmail.com",
     install_requires=["requests", "PyJWT", "requests_oauthlib"],
