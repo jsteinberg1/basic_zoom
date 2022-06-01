96261213681fde679d412ebbd03c52105bfa73f2
Justin Steinberg <jsteinberg@gmail.com>
2022-05-31 21:58:28 -0700

    pretty_print support string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="basic_zoom",
-    version="0.0.7",
+    version="0.0.8",
     author="Justin Steinberg",
     author_email="jsteinberg@gmail.com",
     install_requires=["requests", "PyJWT", "requests_oauthlib", "rich"],
