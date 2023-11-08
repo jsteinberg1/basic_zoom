f152ee17ab9120cf0251a0083871533f3e80d3b3
Justin Steinberg <jsteinberg@gmail.com>
2023-11-07 21:52:31 -0500

    version 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="basic_zoom",
-    version="0.0.10",
+    version="0.0.11",
     author="Justin Steinberg",
     author_email="jsteinberg@gmail.com",
     install_requires=["requests", "PyJWT", "requests_oauthlib", "rich"],
